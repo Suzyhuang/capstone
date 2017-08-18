3d2d32013d2e007aa43030325bc4598e5ee6c052
Jessica Lee <jessicalee@Jessicas-MacBook-Pro-3.local>
2017-08-17 20:57:28 -0700

    outputs one event at a time

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var sanFran = {lat: 37.7749, lng: -122.4194};
 
 var body = document.body;
 
+// const main = document.getElementById('main');
+
 var mainDiv = document.createElement('div');
 mainDiv.id = 'main';
 body.appendChild(mainDiv);
@@ -18,6 +20,35 @@ eventTitleDiv.className = "eventTitle" //to make things different font
 let eventsDiv = document.createElement('div');
 eventContainer.appendChild(eventsDiv);
 
+// var clickEventTest = google.maps.event.addListener(map, 'click', function(event) {
+//         console.log("Latitude: " + event.latLng.lat() + " " + ", longitude: " + event.latLng.lng());
+//         console.log(clickEventTest);
+//         var output = new Object();
+//           output.search = searchInput.value;
+
+//           const xmlHttp = new XMLHttpRequest();
+//           xmlHttp.open("POST", '/events', true);
+//           xmlHttp.setRequestHeader('Content-type', 'application/json');
+//           xmlHttp.onload = function() {
+//             var response = JSON.parse(xmlHttp.response);
+//             const stuff = document.createElement('div');
+//             stuff.id = 'stuff';
+//             if (response == null) {
+//               stuff.textContent += "None";
+//             } else {
+//               var what = response.what;
+//               var type = response.type;
+//               var where = response.where;
+//               stuff.textContent += "I will be "+type + " "+ what +" on "+where;
+//             }
+//             body.appendChild(stuff);
+//           }
+
+//           xmlHttp.send(JSON.stringify(output));
+
+//         });
+//     };
+
 
 // var map1 = google.maps.Map(document.getElementById('map');
 
@@ -26,10 +57,11 @@ function initMap() {
       zoom: 4,
       center: sanFran,
     });
-    var marker = new google.maps.Marker({
-      position: sanFran,
-      map: map
-    });
+    // var marker = new google.maps.Marker({
+    //   position: sanFran,
+    //   map: map
+    // });
+
     // map.addListener('click',function(n){
     //   console.log(n.latLng)
     // })
@@ -40,40 +72,57 @@ function initMap() {
     // var url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=YOUR_API_KEY
         var url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIzaSyDgv5sWpAozwYjqs-bz-nZkHEzFuzD5RNI'
 
+
+        const xttp = new XMLHttpRequest();
+        xttp.open("GET", url, true);
+        xttp.send();
+
+        // var xhr = new XMLHttpRequest();
+        // xhr.open('GET',
+        //   'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&AIzaSyDgv5sWpAozwYjqs-bz-nZkHEzFuzD5RNI');
+        // xhr.setRequestHeader('Authorization',
+        //   'Bearer ' + 'AIzaSyDgv5sWpAozwYjqs-bz-nZkHEzFuzD5RNI');
+        // xhr.send();
+
+
+
+
+
+
     //makes the url to feed to yelp
     
         //sends it to Yelp api to get info
-    const xttp = new XMLHttpRequest();
+    // const xttp = new XMLHttpRequest();
 
-    xttp.onreadystatechange = function() {
-        console.log('hi')
-        if (xttp.readyState===4 && xttp.status ===200) {
-            var res = JSON.parse(xttp.responseText)
-            console.log(res)
-    //     // for ( var i = 0; i<res.length; i++){
-    //     //   var string =  res[i].datetime+res[i].venue.name+res[i].venue.city;
-    //     //   var concertDiv = document.createElement('div');
-    //     //   var textEvent = document.createTextNode(string);
-    //     //   eventDiv.appendChild(textEvent);
-    //     //   eventsDiv.appendChild(concertDiv);
+    // xttp.onreadystatechange = function() {
+    //     console.log('checking state')
+    //     if (xttp.readyState===4 && xttp.status ===200) {
+    //         var res = JSON.parse(xttp.responseText)
+    //         console.log(res)
+    // //     // for ( var i = 0; i<res.length; i++){
+    // //     //   var string =  res[i].datetime+res[i].venue.name+res[i].venue.city;
+    // //     //   var concertDiv = document.createElement('div');
+    // //     //   var textEvent = document.createTextNode(string);
+    // //     //   eventDiv.appendChild(textEvent);
+    // //     //   eventsDiv.appendChild(concertDiv);
 
-        }
-    };
-    // // }
-    xttp.open("GET", url, true);
-    // xttp.setRequestHeader('Access-Control-Allow-Origin','Content-type')
-    // xttp.setRequestHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, content-type, Authorization');
-    // xttp.setRequestHeader('Access-Control-Allow-Origin', '*');
+    //     }
+    // };
+    // // // }
+    // xttp.open("GET", url, true);
+    // // xttp.setRequestHeader('Access-Control-Allow-Origin','Content-type')
+    // // xttp.setRequestHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, content-type, Authorization');
+    // // xttp.setRequestHeader('Access-Control-Allow-Origin', '*');
 
     // xttp.setRequestHeader('Content-Type', 'application/json' );
 
-    // xttp.withCredentials = true;
+    // // xttp.withCredentials = true;
 
-    // console.log(xttp)
-    // xttp.setRequestHeader(, false);
-    // xttp.setHeader('Access-Control-Allow-Headers', 'X-Requested-With', content-type, Authorization)
-    // xttp.setRequestHeader('Authorization', 'Bearer XknB0X4UJx-0D5mSvz3P20P67Ohv4PqZXx5ff3unidzLf7xcPN6Ae3td5hKBfnqEb6B1vPpmRz7EoaqzzU7Lpg52bWzz6WBive2cAe9LPfCLfJtL4NDFrC2BueCVWXYx', false);
-    xttp.send();
+    // // console.log(xttp)
+    // // xttp.setRequestHeader(, false);
+    // // xttp.setHeader('Access-Control-Allow-Headers', 'X-Requested-With', content-type, Authorization)
+    // // xttp.setRequestHeader('Authorization', 'Bearer XknB0X4UJx-0D5mSvz3P20P67Ohv4PqZXx5ff3unidzLf7xcPN6Ae3td5hKBfnqEb6B1vPpmRz7EoaqzzU7Lpg52bWzz6WBive2cAe9LPfCLfJtL4NDFrC2BueCVWXYx', false);
+    // xttp.send();
       // }
       // xmlHttp.send();
           // xmlHttp.send(JSON.stringify(url));
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 /*** Require in modules here ***/
 var express = require('express');
 var app = express();
-var events = require('./data/events.js');
-var activities = require('./data/activities.js');
+var path = require('path');
+var request = require('express');
+
+
+const Factual = require('factual-api');
+const factual = new Factual('Jr4VU8j7IWGNP3P8tg2x21WVC58Opn0w7Zr5EUeo', 'rYkYbju3AROrBb3E4HM9PriEsCfrgzXvoTaQNJet');
+
+
 
 const bodyParser = require('body-parser'); // Uncomment for Bonus Challenge 1
 
@@ -22,12 +28,13 @@ function logreq(req, res, next) {
 }
 
 function sendData(req, res, next) {
-  if (req.url.match('/events')) {
-    res.send(events);
-  }
-  if (req.url.match('/activities')) {
-    res.send(activities);
-  }
+  res.send();
+  // if (req.url.match('/events')) {
+  //   res.send(events);
+  // }
+  // if (req.url.match('/activities')) {
+  //   res.send(activities);
+  // }
 }
 
 function searchData(req, res, next) {
@@ -48,35 +55,30 @@ function searchData(req, res, next) {
       }
     }
   }
-  if (req.url.match('/activities')) {
-    var returnedObject = null;
-    for (let i = 0; i<activities.length;i++){
-      let keyArray = Object.keys(activities[i]);
-      let values = [];
-      for (let k = 0; k<keyArray.length;k++){
-        values.push(activities[i][keyArray[k]]);
-      }
-      for (let j = 0; j<values.length;j++){
-        if (values[j].indexOf(key) != -1) {
-          returnedObject = activities[i];
-        }
-      }
-    }
-  }
+
   res.send(returnedObject);
 }
 
 
 /*** Define routes here ***/
 app.use(express.static(__dirname));
-app.get('/events', logreq, sendData);
-app.get('/activities', logreq, sendData);
+// app.get('/', logreq, sendData);
+// // app.get('/activities', logreq, sendData);
 
-app.post('/events', logreq, searchData);
-app.post('/activities', logreq, searchData);
+// app.post('/', logreq, searchData);
+// app.post('/activities', logreq, searchData);
+app.get('/', function(req,res){
+  res.sendFile(path.join(__dirname + '/capstone.html'));
+});
+
+app.get('/test', function(req, res){
+  res.send('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIzaSyDgv5sWpAozwYjqs-bz-nZkHEzFuzD5RNI')
+  console.log('testing');
+  res.end();
+})
 
 /*** Listen on a port here ***/
 
-app.listen(3000, function() {
-  console.log('listening on port 3000');
+app.listen(5000, function() {
+  console.log('listening on port 5000');
 });
